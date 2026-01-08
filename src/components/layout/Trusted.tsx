3a13a80d2dfa38e88464746dd528dd55e6d1fb1b
Gabriel Silveira <ramos97033@outlook.com>
2026-01-08 11:34:54 -0300

    feat: finishing trusted section

--- src/components/layout/Trusted.tsx
+++ src/components/layout/Trusted.tsx
@@ -3,38 +3,43 @@ const trustedOnes = [
     {
         id:1,
         name:"Google",
+        image:"/google-logo.svg"
     },
     {
         id:2,
-        name:"Microsoft"
+        name:"Microsoft",
+        image:"/microsoft-logo.svg"
     },
     {
         id:3,
-        name:"Tesla"
+        name:"Tesla",
+        image:"/tesla-logo.svg"
     },
     {
         id:4,
-        name:"Amazon"
+        name:"Amazon",
+        image:"/amazon-logo.svg"
     },
     {
         id:5,
-        name:"Meta"
+        name:"Meta",
+        image:"/meta-logo.svg"
     }
 ]
 
 
 const TrustedComponent = () => {
     return(
-        <section className="px-2 py-20 lg:px-20 flex flex-col gap-4">
+        <section className="px-2 py-20 lg:px-20 flex flex-col gap-8 text-white/60">
             <div className="flex justify-evenly items-center ">
-                <div className="flex-1 h-px bg-amber-500 "/>
-                <h1 className="px-6 lg:px-10">Confiado por</h1>
-                <div  className="flex-1 h-px bg-amber-500"/>
+                <div className="flex-1 h-px bg-accent/60"/>
+                <h1 className="px-6 lg:px-10 font-semibold">Confiado por</h1>
+                <div  className="flex-1 h-px bg-accent/60"/>
             </div>
             <div className="flex justify-evenly flex-wrap">
                 {trustedOnes.map((trusted) => {
                     return(
-                        <h1 key={trusted.id} >{trusted.name}</h1>
+                        <img key={trusted.id} src={trusted.image} width={50} className="drop-shadow-xl" alt={trusted.name} />
                     )
                 })}
             </div>
